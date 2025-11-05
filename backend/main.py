725743cefc10338f72516f0f0afa4a63d591ba77
Sala 8 <sala8@FI-LabSistemas.ucatolica.loc>
2025-11-05 12:02:47 -0500

    Main documentado

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
+# Importación del objeto FastAPI
 from fastapi import FastAPI
 
+# Creación de la instancia del objeto
 app = FastAPI()
 
+# READ - Ruta base de hola mundo
 @app.get("/")
 async def holaMundo():
     return {"mensaje": "Hola Mundo!"}
